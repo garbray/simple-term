3e02ba1344b85dae38c1aeff25be2a36957ac750
bryan garzon <bryangarzonb@gmail.com>
2025-01-16 08:43:26 -0500

    add create with fallback

--- lua/simple-term/init.lua
+++ lua/simple-term/init.lua
@@ -1,50 +1,60 @@
 local M = {}
 
-local simpleTerm = vim.api.nvim_create_augroup("SimpleTerm", {})
-
 local terminals = {}
 
 local function create_terminal(create_with)
-    local current_id = vim.api.nvim_get_current_buf()
-    vim.cmd(create_with)
-    local buf_id = vim.api.nvim_get_current_buf()
-    local term_id = vim.b.termial_job_id
+	if not create_with then
+		create_with = ":terminal"
+	end
+	-- log.trace("term: _create_terminal(): Init:", create_with)
+	local current_id = vim.api.nvim_get_current_buf()
 
-    if term_id == nil then
-        return nil
-    end
+	vim.cmd(create_with)
+	local buf_id = vim.api.nvim_get_current_buf()
+	local term_id = vim.b.terminal_job_id
 
-    vim.api.nvim_buf_set_option(buf_id, "swapfile", "hide")
+	if term_id == nil then
+		-- log.error("_create_terminal(): term_id is nil")
+		-- TODO: Throw an error?
+		return
+	end
 
-    vim.api.nvim_buf_current_buf(current_id, "buftype", "terminal")
-    return buf_id, term_id
+	-- Make sure the term buffer has "hidden" set so it doesn't get thrown
+	-- away and cause an error
+	vim.api.nvim_buf_set_option(buf_id, "bufhidden", "hide")
+
+	-- Resets the buffer back to the old one
+	vim.api.nvim_set_current_buf(current_id)
+	return buf_id, term_id
 end
 
 local function find_terminal(args)
-    if type(args) == "number" then
-        args = { idx = args }
-    end
-    local term_handle = terminals[args.idx]
-    -- found a better approach for this
-    if not term_handle or not vim.api.nvim_buf_is_valid(term_handle.buf) then
-        local buf_id, term_id = create_terminal(args.create_with)
-        if buf_id == nil then
-            return
-        end
+	if type(args) == "number" then
+		args = { idx = args }
+	end
 
-        term_handle = {
-            buf_id = buf_id,
-            term_id = term_id,
-        }
-        terminals[args.idx] = term_handle
-    end
-    return term_handle
+	local term_handle = terminals[args.idx]
+	-- found a better approach for this
+	if not term_handle or not vim.api.nvim_buf_is_valid(term_handle.buf_id) then
+		local buf_id, term_id = create_terminal(args.create_with)
+		if buf_id == nil then
+			return
+		end
+
+		term_handle = {
+			buf_id = buf_id,
+			term_id = term_id,
+		}
+		terminals[args.idx] = term_handle
+	end
+	return term_handle
 end
 
+---@param idx number
 function M.goto_terminal(idx)
-    local term_handle = find_terminal(idx)
+	local term_handle = find_terminal(idx)
 
-    vim.api.nvim_set_current_buf(term_handle.buf)
+	vim.api.nvim_set_current_buf(term_handle.buf_id)
 end
 
 return M
